b5a26b49fb8ea1e51e6e41120035bb1c4d95e27e
Akshatha <akshatha@inextlabs.com>
2023-04-18 00:05:33 +0530

    Add project files.

---
+++ AllNetSolutions/Scripts/Process.js
@@ -0,0 +1,77 @@
+﻿
+$(document).ready(function () {
+    var startfirstTime = new Date().getTime(); // record start time
+    $.ajax({
+        url: 'api/process/first',
+        type: 'GET',
+        success: function (data) {
+            if (data.length > 0) {
+                var endTime = new Date().getTime(); // record end time
+                var txt = "";
+                $.each(data, function (i, v) {
+                    txt += '<p>' + v.status + '<p>';
+                    txt += '<p>' + v.timeTaken + '<p>';
+                });
+                var timeTaken = endTime - startfirstTime;
+
+                var t = '<p> Time Taken: ' + timeTaken + 'ms</p>';
+                $('#timeTaken1').append(t);
+                $('#result1').append(txt);
+            }
+            console.log(data);
+            console.timeEnd('myTimer');
+        },
+        error: function (er) {
+        }
+    });
+
+    var startsecondTime = new Date().getTime(); // record start time
+    $.ajax({
+        url: 'api/process/second',
+        type: 'GET',
+        success: function (data) {
+            if (data.length > 0) {
+                var endTime = new Date().getTime(); // record end time
+                var txt = "";
+                $.each(data, function (i, v) {
+                    txt += '<p>' + v.status + '<p>';
+                    txt += '<p>' + v.timeTaken + '<p>';
+                });
+                var timeTaken = endTime - startsecondTime;
+
+                var t = '<p> Time Taken: ' + timeTaken + 'ms</p>';
+                $('#timeTaken2').append(t);
+                $('#result2').append(txt);
+            }
+            console.log(data);
+            console.timeEnd('myTimer');
+        },
+        error: function (er) {
+        }
+    });
+
+    var startTthirdime = new Date().getTime(); // record start time
+    $.ajax({
+        url: 'api/process/third',
+        type: 'GET',
+        success: function (data) {
+            if (data.length > 0) {
+                var endTime = new Date().getTime(); // record end time
+                var txt = "";
+                $.each(data, function (i, v) {
+                    txt += '<p>' + v.status + '<p>';
+                    txt += '<p>' + v.timeTaken + '<p>';
+                });
+                var timeTaken = endTime - startTthirdime;
+
+                var t = '<p> Time Taken: ' + timeTaken + 'ms</p>';
+                $('#timeTaken3').append(t);
+                $('#result3').append(txt);
+            }
+            console.log(data);
+            console.timeEnd('myTimer');
+        },
+        error: function (er) {
+        }
+    });
+});
